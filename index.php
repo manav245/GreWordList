39f8ec56edcac412b62ceb9d748d62edfb687523
Manav <manav245@gmail.com>
2015-10-10 22:18:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$file = fopen("list.txt", "r") or die("Unable to open file!");
+$list = array();
+while(!feof($file)){
+    $line = fgets($file);
+    $list[] = $line;
+}
+foreach ($list as $x) {
+	echo $x;
+	}
+fclose($file);
+?>
